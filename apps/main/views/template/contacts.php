9fa5b4ed3becbd54b6a6d4372f4995de2dda1b10
Loginnn <loginnns4@gmail.com>
2017-08-14 14:18:17 +0300

    Страница contacts

---
+++ apps/main/views/template/contacts.php
@@ -0,0 +1,45 @@
+<?php
+  //google maps, пришлось править AutoAssets для этого
+  $assets->addJs('https://maps.google.com/maps/api/js?sensor=false');
+  $assets->addJs('https://vk.com/js/api/openapi.js');
+?>
+<div class="page__contacts container-fluid">
+  <div class="c__breadcrumbs row">
+    <div class="c__inner container-fluid">
+      <ul class="breadcrumbs">
+        <li><a href="#">Главная</a></li>
+        <li><h1>Контакты</h1></li>
+      </ul>
+    </div>
+  </div>
+  
+  <div class="map-header row">
+    <div class="c__inner container-fluid">
+    <ul class="headers">
+      <li class="active"><a href="#">Москва</a></li>
+      <li><a href="#">Санкт-Петербург</a></li>
+      <li><a href="#">Нижний-Новгород</a></li>
+    </ul>
+    </div>
+  </div>
+  <div class="map row" data-role="map">
+    
+  </div>
+  <div class="socials row">
+    <div class="c__inner container-fluid">
+      <h1 class="caption">Присоединяйтесь к нам</h1>
+      <div class="blocks-container">
+        <div class="social-block">
+          <div id="vk_widget"></div>
+        </div>
+        <div class="social-block" id="fb-root">
+          <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-small-header="false" data-width="386" data-height="400" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- это нужно дял работы Facebook виджета -->
+<div id="fb-root"></div>
+<!---->
+<script type="text/javascript" src=""></script>
---
+++ web/frontend/contacts/contacts.js
@@ -0,0 +1,18 @@
+"use strict";
+$(document).ready(function() {
+  var map = new google.maps.Map($('*[data-role="map"]')[0],
+  {
+    zoom: 8,
+    center: new google.maps.LatLng(30.0, 30.0),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+  });
+  VK.Widgets.Group("vk_widget", {mode: 0, width: "auto", height:400, wide:1}, 20003922);
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v2.10&appId=752583108171786";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+});
